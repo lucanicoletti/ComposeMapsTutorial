58723c9efed1bc744f15931464682899af47f417
Luca Nicoletti <luca.nicolett@gmail.com>
2024-01-07 09:04:50 +0000

    Clustering without actions

--- app/src/main/java/com/lucanicoletti/ComposeMapsTutorial/MainActivity.kt
+++ app/src/main/java/com/lucanicoletti/ComposeMapsTutorial/MainActivity.kt
@@ -3,33 +3,22 @@ package com.lucanicoletti.ComposeMapsTutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.clustering.ClusterItem
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.MarkerInfoWindowContent
-import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.clustering.Clustering
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.lucanicoletti.ComposeMapsTutorial.ui.theme.ComposeMapsTutorialTheme
 
@@ -96,40 +85,15 @@ class MainActivity : ComponentActivity() {
                         properties = mapProperties,
                         uiSettings = mapUiSettings,
                     ) {
-                        for (data in markersData) {
-                            MarkerInfoWindowContent(
-                                state = MarkerState(
-                                    position = data.location
-                                ),
-                                title = data.title,
-                                snippet = data.description,
-                            ) {
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(32.dp)
-                                ) {
-                                    Text(
-                                        modifier = Modifier.padding(top = 6.dp),
-                                        text = data.title,
-                                        fontWeight = FontWeight.Bold,
-                                        color = Color.Black,
-                                    )
-                                    data.description?.let { desc -> Text(desc) }
-                                    data.imageResourceId?.let { res ->
-                                        Image(
-                                            modifier = Modifier
-                                                .padding(top = 6.dp)
-                                                .size(240.dp),
-                                            painter = painterResource(id = res),
-                                            contentDescription = data.description
-                                        )
-                                    }
-                                }
-
-                            }
-                        }
+                        Clustering(
+                            items = markersData,
+                            onClusterClick = {
+                                false
+                            },
+                            onClusterItemClick = { _ ->
+                                false
+                            },
+                        )
                     }
                 }
             }
@@ -140,31 +104,31 @@ class MainActivity : ComponentActivity() {
 private val markersData = listOf(
     MarkerData(
         location = LatLng(51.508021, -0.075971),
-        title = "Tower of London",
+        name = "Tower of London",
         description = "Nearby Tower Bridge, a beautiful castle in Central London.",
         imageResourceId = R.drawable.tower,
     ),
     MarkerData(
         location = LatLng(51.517814, -0.1270),
-        title = "British Museum",
+        name = "British Museum",
         description = "The most famous museum in the city.",
         imageResourceId = R.drawable.museum,
     ),
     MarkerData(
         location = LatLng(51.503333, -0.119664),
-        title = "London Eye",
+        name = "London Eye",
         description = "The highest wheel, the iconic Coca-Cola attraction.",
         imageResourceId = R.drawable.eye,
     ),
     MarkerData(
         location = LatLng(51.50082, -0.143016),
-        title = "Buckingham Palace",
+        name = "Buckingham Palace",
         description = "Where the Royal Family lives.",
         imageResourceId = R.drawable.palace,
     ),
     MarkerData(
         location = LatLng(51.532924, -0.10584),
-        title = "Angel Station",
+        name = "Angel Station",
         description = "A well-known station in London City Centre.",
         imageResourceId = R.drawable.angel,
     ),
@@ -173,7 +137,16 @@ private val markersData = listOf(
 
 data class MarkerData(
     val location: LatLng,
-    val title: String,
+    val name: String,
     val description: String?,
     val imageResourceId: Int? = null,
-)
+) : ClusterItem {
+    override fun getPosition(): LatLng = location
+
+    override fun getTitle(): String? = name
+
+    override fun getSnippet(): String? = description
+
+    override fun getZIndex(): Float? = 1f
+
+}
