0fd29f50dd41cc8e91b3879a6722181144b9461c
Luca Nicoletti <luca.nicolett@gmail.com>
2024-01-13 10:07:28 +0000

    Custom clustering data

--- app/src/main/java/com/lucanicoletti/ComposeMapsTutorial/MainActivity.kt
+++ app/src/main/java/com/lucanicoletti/ComposeMapsTutorial/MainActivity.kt
@@ -3,17 +3,34 @@ package com.lucanicoletti.ComposeMapsTutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterItem
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
@@ -95,12 +112,73 @@ class MainActivity : ComponentActivity() {
                                 )
                                 false
                             },
-                            onClusterItemClick = { _ ->
-                                false
-                            },
+                            onClusterItemClick = { _ -> false },
+                            clusterContent = { CustomClusterContent(cluster = it) },
+                            clusterItemContent = { CustomClusterContentItem(data = it) }
                         )
                     }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomClusterContent(cluster: Cluster<MarkerData>) {
+    val size = cluster.size
+    Surface(
+        modifier = Modifier
+            .width(40.dp)
+            .height(20.dp),
+        shape = RoundedCornerShape(8.dp),
+        color = Color.Blue,
+        contentColor = Color.White,
+        border = BorderStroke(1.dp, Color.White)
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                size.toString(),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Black,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+fun CustomClusterContentItem(data: MarkerData) {
+    Surface(
+        modifier = Modifier
+            .width(80.dp)
+            .height(40.dp),
+        shape = RoundedCornerShape(8.dp),
+        color = Color.Red,
+        contentColor = Color.White,
+        border = BorderStroke(1.dp, Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(2.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                data.name,
+                fontSize = 12.sp,
+                lineHeight = 12.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            data.snippet?.let {
+                Text(
+                    modifier = Modifier.padding(top = 2.dp),
+                    text = it,
+                    fontSize = 8.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontWeight = FontWeight.Light,
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
