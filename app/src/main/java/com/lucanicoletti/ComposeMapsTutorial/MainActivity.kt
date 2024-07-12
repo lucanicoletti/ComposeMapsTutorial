5d49159feb6f20e70be700d214febf2d5672221e
Luca Nicoletti <luca.nicolett@gmail.com>
2024-07-12 16:05:18 +0100

    Add clustering data

--- app/src/main/java/com/lucanicoletti/ComposeMapsTutorial/MainActivity.kt
+++ app/src/main/java/com/lucanicoletti/ComposeMapsTutorial/MainActivity.kt
@@ -3,16 +3,9 @@ package com.lucanicoletti.ComposeMapsTutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
@@ -20,26 +13,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterItem
+import com.google.maps.android.clustering.algo.NonHierarchicalDistanceBasedAlgorithm
 import com.google.maps.android.clustering.algo.NonHierarchicalViewBasedAlgorithm
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.clustering.Clustering
 import com.google.maps.android.compose.clustering.rememberClusterManager
+import com.google.maps.android.compose.clustering.rememberClusterRenderer
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.lucanicoletti.ComposeMapsTutorial.ui.theme.ComposeMapsTutorialTheme
+import java.util.Locale
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -93,17 +88,36 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         cameraPositionState = cameraPositionState,
                     ) {
+
                         val clusterManager = rememberClusterManager<ClusterItemImpl>()
 
-                        // Here the clusterManager is being customized with a NonHierarchicalViewBasedAlgorithm.
-                        // This speeds up by a factor the rendering of items on the screen.
-                        clusterManager?.setAlgorithm(
-                            NonHierarchicalViewBasedAlgorithm(
+                        val nonHierarchicalViewBasedAlgorithm =
+                            NonHierarchicalViewBasedAlgorithm<ClusterItemImpl>(
                                 screenWidth.value.toInt(),
                                 screenHeight.value.toInt()
                             )
-                        )
+                        val distanceBasedAlgorithm =
+                            NonHierarchicalDistanceBasedAlgorithm<ClusterItemImpl>().apply {
+                                setMaxDistanceBetweenClusteredItems(200)
+                            }
+
+                        clusterManager?.algorithm = nonHierarchicalViewBasedAlgorithm
+
                         if (clusterManager != null) {
+                            val renderer = rememberClusterRenderer(
+                                clusterManager = clusterManager,
+                                clusterContent = { cluster ->
+                                    IconAsClusterContent(
+                                        cluster = cluster
+                                    )
+                                },
+                                clusterItemContent = { IconAsClusterContentItem(data = it) },
+                            )
+                            SideEffect {
+                                if (clusterManager?.renderer != renderer) {
+                                    clusterManager?.renderer = renderer ?: return@SideEffect
+                                }
+                            }
                             Clustering(items = markerItems, clusterManager = clusterManager)
                         }
                     }
@@ -114,16 +128,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun IconAsClusterContentItem(data: MarkerData) {
+fun IconAsClusterContentItem(data: ClusterItemImpl) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = data.name,
-            color = Color.Red,
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp,
-        )
+        data.title?.let {
+            Text(
+                text = it,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+            )
+        }
         Icon(
             modifier = Modifier.size(32.dp),
             imageVector = Icons.Default.LocationOn,
@@ -134,12 +150,12 @@ fun IconAsClusterContentItem(data: MarkerData) {
 }
 
 @Composable
-fun IconAsClusterContent(cluster: Cluster<MarkerData>) {
+fun IconAsClusterContent(cluster: Cluster<ClusterItemImpl>) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = String.format("%d elements", cluster.size),
+            text = String.format(Locale.getDefault(), "%d elements", cluster.size),
             color = Color.Blue,
             fontWeight = FontWeight.Bold,
             fontSize = 16.sp,
@@ -153,99 +169,6 @@ fun IconAsClusterContent(cluster: Cluster<MarkerData>) {
     }
 }
 
-@Composable
-fun CustomClusterContent(cluster: Cluster<MarkerData>) {
-    val size = cluster.size
-    Surface(
-        modifier = Modifier.size(40.dp),
-        shape = CircleShape,
-        color = Color.Blue,
-        contentColor = Color.White,
-        border = BorderStroke(1.dp, Color.White)
-    ) {
-        Box(contentAlignment = Alignment.Center) {
-            Text(
-                size.toString(),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Black,
-                textAlign = TextAlign.Center
-            )
-        }
-    }
-}
-
-@Composable
-fun CustomClusterContentItem(data: MarkerData) {
-    Surface(
-        modifier = Modifier
-            .width(80.dp)
-            .height(40.dp),
-        shape = RoundedCornerShape(8.dp),
-        color = Color.Red,
-        contentColor = Color.White,
-        border = BorderStroke(1.dp, Color.White)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(2.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                data.name,
-                fontSize = 12.sp,
-                lineHeight = 12.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            )
-            data.snippet?.let {
-                Text(
-                    modifier = Modifier.padding(top = 2.dp),
-                    text = it,
-                    fontSize = 8.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    fontWeight = FontWeight.Light,
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-    }
-}
-
-private val markersData = listOf(
-    MarkerData(
-        location = LatLng(51.508021, -0.075971),
-        name = "Tower of London",
-        description = "Nearby Tower Bridge, a beautiful castle in Central London.",
-        imageResourceId = R.drawable.tower,
-    ),
-    MarkerData(
-        location = LatLng(51.503333, -0.119664),
-        name = "London Eye",
-        description = "The highest wheel, the iconic Coca-Cola attraction.",
-        imageResourceId = R.drawable.eye,
-    ),
-    MarkerData(
-        location = LatLng(51.50082, -0.143016),
-        name = "Buckingham Palace",
-        description = "Where the Royal Family lives.",
-        imageResourceId = R.drawable.palace,
-    ),
-    MarkerData(
-        location = LatLng(51.517814, -0.1270),
-        name = "British Museum",
-        description = "The most famous museum in the city.",
-        imageResourceId = R.drawable.museum,
-    ),
-    MarkerData(
-        location = LatLng(51.532924, -0.10584),
-        name = "Angel Station",
-        description = "A well-known station in London City Centre.",
-        imageResourceId = R.drawable.angel,
-    ),
-)
-
-
 data class ClusterItemImpl(val location: LatLng) : ClusterItem {
     override fun getPosition(): LatLng = location
 
@@ -254,21 +177,4 @@ data class ClusterItemImpl(val location: LatLng) : ClusterItem {
     override fun getSnippet(): String? = null
 
     override fun getZIndex(): Float? = null
-}
-
-
-data class MarkerData(
-    val location: LatLng,
-    val name: String,
-    val description: String?,
-    val imageResourceId: Int? = null,
-) : ClusterItem {
-    override fun getPosition(): LatLng = location
-
-    override fun getTitle(): String? = name
-
-    override fun getSnippet(): String? = description
-
-    override fun getZIndex(): Float? = 1f
-
 }
